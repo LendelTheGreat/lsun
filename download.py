f12e3145710b0f75ff33222ee602ee4b52678f18
Tomas <tomas@intuitionmachines.com>
2020-10-23 15:54:27 +0000

    Ability to download from objects super-category

--- download.py
+++ download.py
@@ -2,7 +2,8 @@
 
 from __future__ import print_function, division
 import argparse
-from os.path import join
+from os import makedirs
+from os.path import join, dirname
 
 import subprocess
 from urllib.request import Request, urlopen
@@ -18,23 +19,34 @@ def list_categories():
         return response.read().decode().strip().split('\n')
 
 
-def download(out_dir, category, set_name):
-    url = 'http://dl.yf.io/lsun/scenes/{category}_' \
-          '{set_name}_lmdb.zip'.format(**locals())
+def download(out_dir, super_category, category, set_name):
+    suffix = '.zip'
+    if super_category == 'scenes':
+        suffix = f'_{set_name}_lmdb.zip'
+    url = f'http://dl.yf.io/lsun/{super_category}/{category}{suffix}'
+          
     if set_name == 'test':
-        out_name = 'test_lmdb.zip'
-        url = 'http://dl.yf.io/lsun/scenes/{set_name}_lmdb.zip'
+        assert super_category == 'scenes', f'Test set is only available for the scenes super_category!'
+        out_name = '{super_category}/test_lmdb.zip'.format(**locals())
+        url = 'http://dl.yf.io/lsun/{super_category}/{set_name}_lmdb.zip'
     else:
-        out_name = '{category}_{set_name}_lmdb.zip'.format(**locals())
+        if super_category == 'scenes':
+            out_name = '{super_category}/{category}_{set_name}_lmdb.zip'.format(**locals())
+        elif super_category == 'objects':
+            out_name = '{super_category}/{category}.zip'.format(**locals())
+        else:
+            raise ValueError(f'super_category should be in ["scenes", "objects"] but is {super_category}')
     out_path = join(out_dir, out_name)
     cmd = ['curl', url, '-o', out_path]
-    print('Downloading', category, set_name, 'set')
+    print(f'Downloading super_category "{super_category}" category "{category}" set "{set_name}" with cmd: {cmd}')
+    makedirs(dirname(out_path), exist_ok=True)
     subprocess.call(cmd)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--out_dir', default='')
+    parser.add_argument('-s', '--super_category', default='scenes', help='"scenes" or "objects"')
     parser.add_argument('-c', '--category', default=None)
     args = parser.parse_args()
 
@@ -42,17 +54,22 @@ def main():
     if args.category is None:
         print('Downloading', len(categories), 'categories')
         for category in categories:
-            download(args.out_dir, category, 'train')
-            download(args.out_dir, category, 'val')
+            download(args.out_dir, args.super_category, args.category, 'train')
+            download(args.out_dir, args.super_category, category, 'val')
         download(args.out_dir, '', 'test')
     else:
-        if args.category == 'test':
-            download(args.out_dir, '', 'test')
-        elif args.category not in categories:
-            print('Error:', args.category, "doesn't exist in", 'LSUN release')
+        if args.super_category == 'scenes':
+            if args.category == 'test':
+                download(args.out_dir, args.super_category, '', 'test')
+            elif args.category not in categories:
+                print('Error:', args.category, "doesn't exist in", 'LSUN release')
+            else:
+                download(args.out_dir, args.super_category, args.category, 'train')
+                download(args.out_dir, args.super_category, args.category, 'val')
+        elif args.super_category == 'objects':
+            download(args.out_dir, args.super_category, args.category, '')
         else:
-            download(args.out_dir, args.category, 'train')
-            download(args.out_dir, args.category, 'val')
+            raise ValueError(f'super_category should be in ["scenes", "objects"] but is {args.super_category}')
 
 
 if __name__ == '__main__':
---
+++ lsun_stats.md
@@ -0,0 +1,41 @@
+* table below obtained from [GenForce](https://github.com/genforce/genforce) 
+
+
+Statistical information of [LSUN](https://github.com/fyu/lsun) dataset is summarized as follows:
+
+| LSUN Datasets Stats    |                   |      |
+| :--                    | :--:              | :--: |
+| Name                   | Number of Samples | Size |
+| *Scenes*
+| bedroom (train)        |         3,033,042 |  43G |
+| bridge (train)         |           818,687 |  15G |
+| churchoutdoor (train)  |           126,227 |   2G |
+| classroom (train)      |           168,103 |   3G |
+| conferenceroom (train) |           229,069 |   4G |
+| diningroom (train)     |           657,571 |  11G |
+| kitchen (train)        |         2,212,277 |  33G |
+| livingroom (train)     |         1,315,802 |  21G |
+| restaurant (train)     |           626,331 |  13G |
+| tower (train)          |           708,264 |  11G |
+| *Objects*
+| airplane               |         1,530,696 |  34G |
+| bicycle                |         3,347,211 | 129G |
+| bird                   |         2,310,362 |  65G |
+| boat                   |         2,651,165 |  86G |
+| bottle                 |         3,202,760 |  64G |
+| bus                    |           695,891 |  24G |
+| car                    |         5,520,756 | 173G |
+| cat                    |         1,657,266 |  42G |
+| chair                  |         5,037,807 | 116G |
+| cow                    |           377,379 |  15G |
+| diningtable            |         1,537,123 |  48G |
+| dog                    |         5,054,817 | 145G |
+| horse                  |         2,000,340 |  69G |
+| motorbike              |         1,194,101 |  42G |
+| person                 |        18,890,816 | 477G |
+| pottedplant            |         1,104,859 |  43G |
+| sheep                  |           418,983 |  18G |
+| sofa                   |         2,365,870 |  56G |
+| train                  |         1,148,020 |  43G |
+| tvmonitor              |         2,463,284 |  46G |
+
